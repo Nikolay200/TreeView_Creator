51c54048b71c48c009e788a840a59c56b02db480
Nikolay Smirnov <nikolay.smirnov@autogramma.ru>
2024-08-22 15:49:50 +0300

    Add file explorer

--- TreeViewCreator_AvaloniaApp/ViewModels/MainViewModel.cs
+++ TreeViewCreator_AvaloniaApp/ViewModels/MainViewModel.cs
@@ -1,6 +1,49 @@
-﻿namespace TreeViewCreator_AvaloniaApp.ViewModels;
+﻿using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Platform.Storage;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TreeViewCreator_AvaloniaApp.ViewModels;
 
 public partial class MainViewModel : ViewModelBase
 {
-    public string Greeting => "Welcome to Avalonia!";
+    private string fileName;
+    public string FileName
+    {
+        get { return fileName; }
+        set
+        {
+            fileName = value;
+            OnPropertyChanged();
+        }
+    }
+
+    private IStorageFolder folder;
+
+    [RelayCommand]
+    private async Task LoadDirectory()
+    {
+        var folder = await DoOpenFilePickerAsync();
+        if (folder is null) return;
+        this.folder = folder;
+        FileName = folder.Path.AbsolutePath;
+    }
+    private async Task<IStorageFolder?> DoOpenFilePickerAsync()
+    {
+        if (Application.Current?.ApplicationLifetime is not IClassicDesktopStyleApplicationLifetime desktop ||
+            desktop.MainWindow?.StorageProvider is not { } provider)
+            throw new NullReferenceException("Missing StorageProvider instance.");
+
+        var folder = await provider.OpenFolderPickerAsync(new FolderPickerOpenOptions()
+        {
+            Title = "File Explorer",
+            AllowMultiple = false,
+        });
+
+        return folder[0];
+    }
+
 }
