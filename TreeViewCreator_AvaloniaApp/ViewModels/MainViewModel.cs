133cfaf05f0d6620f6d35f8945b5d230a1870b83
Nikolay Smirnov <nikolay.smirnov@autogramma.ru>
2024-08-22 16:27:55 +0300

    Add load directory panel

--- TreeViewCreator_AvaloniaApp/ViewModels/MainViewModel.cs
+++ TreeViewCreator_AvaloniaApp/ViewModels/MainViewModel.cs
@@ -1,35 +1,76 @@
 ﻿using Avalonia;
+using Avalonia.Controls;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Platform.Storage;
 using CommunityToolkit.Mvvm.Input;
 using System;
-using System.Linq;
+using System.Collections.ObjectModel;
 using System.Threading.Tasks;
+using System.Xml.Linq;
+using TreeViewCreator_AvaloniaApp.Models;
 
 namespace TreeViewCreator_AvaloniaApp.ViewModels;
 
+//https://www.c-sharpcorner.com/article/display-sub-directories-and-files-in-treeview/
 public partial class MainViewModel : ViewModelBase
 {
-    private string fileName;
-    public string FileName
+    private string folderDirectory;
+    public string FolderDirectory
     {
-        get { return fileName; }
+        get { return folderDirectory; }
         set
         {
-            fileName = value;
+            folderDirectory = value;
+            OnPropertyChanged();
+        }
+    }
+
+    private int progress;
+    public int Progress
+    {
+        get { return progress; }
+        set
+        {
+            progress = value;
             OnPropertyChanged();
         }
     }
 
     private IStorageFolder folder;
+    public ObservableCollection<Node> Nodes { get; }
+
+
+    public MainViewModel()
+    {
+    //https://docs.avaloniaui.net/docs/reference/controls/treeview-1
+        Nodes = new ObservableCollection<Node>
+            {
+                new Node("Animals", new ObservableCollection<Node>
+                {
+                    new Node("Mammals", new ObservableCollection<Node>
+                    {
+                        new Node("Lion"), new Node("Cat"), new Node("Zebra")
+                    })
+                })
+            };
+    }
 
     [RelayCommand]
-    private async Task LoadDirectory()
+    private async Task SelectDirectory()
     {
         var folder = await DoOpenFilePickerAsync();
         if (folder is null) return;
         this.folder = folder;
-        FileName = folder.Path.AbsolutePath;
+        FolderDirectory = folder.Path.AbsolutePath;
+    }
+
+    [RelayCommand]
+    private async Task LoadDirectory()
+    {
+        Progress = 0;
+        Nodes.Clear();
+
+        if(string.IsNullOrEmpty(FolderDirectory))
     }
     private async Task<IStorageFolder?> DoOpenFilePickerAsync()
     {
