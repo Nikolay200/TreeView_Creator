cc0a3071df1966529afbf07b9e9d1ec28ba810e3
Nikolay Smirnov <nikolay.smirnov@autogramma.ru>
2024-08-23 12:44:07 +0300

    Base concept

--- TreeViewCreator_AvaloniaApp/Models/Node.cs
+++ TreeViewCreator_AvaloniaApp/Models/Node.cs
@@ -6,15 +6,43 @@ namespace TreeViewCreator_AvaloniaApp.Models
     {
         public ObservableCollection<Node>? SubNodes { get; }
         public string Title { get; }
+        public int ImageIndex {  get; }
+        public int SelectedImageIndex {  get; }
+        public string FullPath { get; set; }
+        public int Size {  get; set; }
+        public Node Parent { get; }
 
         public Node(string title)
         {
             Title = title;
         }
-
         public Node(string title, ObservableCollection<Node> subNodes)
         {
             Title = title;
+            SubNodes = subNodes;
+        }
+        public Node(string title, int imageIndex, ObservableCollection<Node> subNodes)
+        {
+            Title = title;
+            ImageIndex = imageIndex;
+            SubNodes = subNodes;
+        }
+        public Node(string title, int imageIndex)
+        {
+            Title = title;
+            ImageIndex = imageIndex;
+        }
+        public Node(string title, int imageIndex, int selectedImageIndex)
+        {
+            Title = title;
+            ImageIndex = imageIndex;
+            SelectedImageIndex = selectedImageIndex;
+        }
+        public Node(string title, int imageIndex, int selectedImageIndex, ObservableCollection<Node> subNodes)
+        {
+            Title = title;
+            ImageIndex = imageIndex;
+            SelectedImageIndex = selectedImageIndex;
             SubNodes = subNodes;
         }
     }
--- TreeViewCreator_AvaloniaApp/ViewModels/MainViewModel.cs
+++ TreeViewCreator_AvaloniaApp/ViewModels/MainViewModel.cs
@@ -1,12 +1,14 @@
 ﻿using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Controls.Models.TreeDataGrid;
 using Avalonia.Platform.Storage;
 using CommunityToolkit.Mvvm.Input;
 using System;
 using System.Collections.ObjectModel;
+using System.IO;
+using System.Text;
 using System.Threading.Tasks;
-using System.Xml.Linq;
 using TreeViewCreator_AvaloniaApp.Models;
 
 namespace TreeViewCreator_AvaloniaApp.ViewModels;
@@ -25,34 +27,56 @@ public partial class MainViewModel : ViewModelBase
         }
     }
 
-    private int progress;
-    public int Progress
+    private int progressValue;
+    public int ProgressValue
     {
-        get { return progress; }
+        get { return progressValue; }
         set
         {
-            progress = value;
+            progressValue = value;
+            OnPropertyChanged();
+        }
+    }
+    private int maximum;
+    public int Maximum
+    {
+        get { return maximum; }
+        set
+        {
+            maximum = value;
+            OnPropertyChanged();
+        }
+    }
+    private int minimum;
+    public int Minimum
+    {
+        get { return minimum; }
+        set
+        {
+            minimum = value;
             OnPropertyChanged();
         }
     }
 
     private IStorageFolder folder;
-    public ObservableCollection<Node> Nodes { get; }
-
-
+    public ObservableCollection<Node> Nodes { get; set; } = new ObservableCollection<Node>();
+    public HierarchicalTreeDataGridSource<Node> Source { get; set; }
+    public ObservableCollection<Node> SelectedNodes { get; } 
     public MainViewModel()
     {
-    //https://docs.avaloniaui.net/docs/reference/controls/treeview-1
-        Nodes = new ObservableCollection<Node>
+        //https://docs.avaloniaui.net/docs/reference/controls/treeview-1
+
+        Source = new HierarchicalTreeDataGridSource<Node>(Nodes)
+        {
+            Columns =
             {
-                new Node("Animals", new ObservableCollection<Node>
-                {
-                    new Node("Mammals", new ObservableCollection<Node>
-                    {
-                        new Node("Lion"), new Node("Cat"), new Node("Zebra")
-                    })
-                })
-            };
+                new HierarchicalExpanderColumn<Node>(
+                    new TextColumn<Node, string>("Название", x => x.Title),
+                    x => x.SubNodes),
+                new TextColumn<Node, string>("Директория", x => x.FullPath),
+                new TextColumn<Node, int>("Размер", x => x.Size),
+            },
+        };
     }
 
     [RelayCommand]
@@ -61,17 +85,82 @@ public partial class MainViewModel : ViewModelBase
         var folder = await DoOpenFilePickerAsync();
         if (folder is null) return;
         this.folder = folder;
-        FolderDirectory = folder.Path.AbsolutePath;
+        FolderDirectory = folder.Path.LocalPath;
     }
 
     [RelayCommand]
     private async Task LoadDirectory()
     {
-        Progress = 0;
-        Nodes.Clear();
+        ProgressValue = 0;
+        //Nodes.Clear();
 
-        if(string.IsNullOrEmpty(FolderDirectory))
+        if (string.IsNullOrEmpty(FolderDirectory))
+        {
+            return;
+        }
+        else if(Directory.Exists(FolderDirectory))
+        {
+            LoadSelectedDirectory(FolderDirectory);
+        }
     }
+
+    private void LoadSelectedDirectory(string folderDirectory)
+    {
+        DirectoryInfo di = new DirectoryInfo(folderDirectory);
+        //Setting ProgressBar Maximum Value
+        //Maximum = Directory.GetFiles(folderDirectory, "*.*", SearchOption.AllDirectories).Length + 
+        //    Directory.GetDirectories(folderDirectory, "**", SearchOption.AllDirectories).Length;
+
+        var treeNode = new Node(di.Name, 0, new ObservableCollection<Node>());        
+        treeNode.FullPath = di.FullName;            
+        Nodes.Add(treeNode);
+
+        LoadFiles(folderDirectory, treeNode);
+        LoadSubDirectories(folderDirectory, treeNode);
+    }
+    private void LoadSubDirectories(string dir, Node node)
+    {
+        // Get all subdirectories
+        string[] subdirectoryEntries = Directory.GetDirectories(dir);
+        // Loop through them to see if they have any other subdirectories
+        foreach (string subdirectory in subdirectoryEntries)
+        {
+            DirectoryInfo di = new DirectoryInfo(subdirectory);
+            var treeNode = new Node(di.Name, 0, new ObservableCollection<Node>());
+            treeNode.FullPath = di.FullName;
+            node.SubNodes.Add(treeNode);
+            LoadFiles(subdirectory, treeNode);
+            LoadSubDirectories(subdirectory, treeNode);
+            //UpdateProgress();
+        }
+    }
+    private void LoadFiles(string dir, Node node)
+    {
+        string[] Files = Directory.GetFiles(dir, "*.*");
+        // Loop through them to see files
+        foreach (string file in Files)
+        {
+            FileInfo fi = new FileInfo(file);
+            var treeNode = new Node(fi.Name, 1);
+            treeNode.FullPath = fi.FullName;
+            node.SubNodes.Add(treeNode);
+            //UpdateProgress();
+        }
+    }
+
+    private void UpdateProgress()
+    {
+        if (ProgressValue < Maximum)
+        {
+            ProgressValue++;
+            int percent = (int)(((double)ProgressValue / (double)Maximum) * 100);
+            //progressBar1.CreateGraphics().DrawString(percent.ToString() + "%", 
+            //    new Font("Arial", (float)8.25, FontStyle.Regular), 
+            //    Brushes.Black, 
+            //    new PointF(progressBar1.Width / 2 - 10, progressBar1.Height / 2 - 7));            
+        }
+    }
+
     private async Task<IStorageFolder?> DoOpenFilePickerAsync()
     {
         if (Application.Current?.ApplicationLifetime is not IClassicDesktopStyleApplicationLifetime desktop ||
@@ -81,7 +170,7 @@ public partial class MainViewModel : ViewModelBase
         var folder = await provider.OpenFolderPickerAsync(new FolderPickerOpenOptions()
         {
             Title = "File Explorer",
-            AllowMultiple = false,
+            AllowMultiple = false
         });
 
         return folder[0];
